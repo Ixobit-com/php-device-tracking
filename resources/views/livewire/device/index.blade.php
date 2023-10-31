59038cc2978399d30b9a62253a9713a43e0eca7e
Alexander Bakharev <alexander.bakharev@sibers.com>
2023-10-31 11:20:01 +0600

    added check for no devices

--- resources/views/livewire/device/index.blade.php
+++ resources/views/livewire/device/index.blade.php
@@ -8,6 +8,7 @@
                            class="mt-1 block w-full bg-white border border-gray-300 rounded-md py-3 px-4 leading-tight focus:outline-none focus:border-blue-500">
                 </label>
             </div>
+            @if(count($devices) > 0)
             <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
                 @foreach($devices as $device)
                     <div class="bg-white rounded-lg shadow-md">
@@ -45,6 +46,9 @@
                     </div>
                 @endforeach
             </div>
+            @else
+                <div>No devices found.</div>
+            @endif
             {{ $devices->links('livewire::tailwind') }}
         </div>
         <div class="w-1/4">
